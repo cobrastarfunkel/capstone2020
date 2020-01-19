f8d5a8d61df7287c003150f61e869cd92b33f473
cobrastarfunkel <iancobia@gmail.com>
2020-01-19 11:57:26 -0600

    Removed uneeded tests

--- gradle_init/src/test/java/databse/SqliteDatabseTest.java
+++ gradle_init/src/test/java/databse/SqliteDatabseTest.java
@@ -8,6 +8,7 @@ import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 
 import org.junit.Test;
 
@@ -19,23 +20,11 @@ public class SqliteDatabseTest {
 	private String dbName = "gradleDb.sqlite";
 	private SqliteDatabase testDb;
 
-	private String testFileName = "scenario1.txt";
-
 	@Test
 	public void testDbCreated() {
 		testDb = new SqliteDatabase(dbName);
 		File dbFile = new File(testDb.getPath());
 		assertTrue("Database Should be created", dbFile.exists());
-
-	}
-
-	@Test
-	public void testScenarioObject() {
-		Scenario sc = new Scenario("scenario1.txt");
-		assertEquals("scenario1.txt", sc.getName());
-		assertEquals(
-				"C:\\eclipse\\work_space\\capstone\\capstone-project-conspicuous\\gradle_init\\src\\main\\resources\\scenarios\\scenario1.txt",
-				sc.getPath());
 
 	}
 
@@ -49,14 +38,18 @@ public class SqliteDatabseTest {
 	@Test
 	public void testScenariosTableCreated() {
 		testDb = new SqliteDatabase(dbName);
+		ArrayList<Scenario> scenarios = testDb.getScenarioTable().getScs();
 
-		String sql = "SELECT filename FROM scenarios";
+		String sql = "SELECT * FROM scenarios";
 
 		try (Connection conn = testDb.connect();
 				Statement stmt = conn.createStatement();
 				ResultSet rs = stmt.executeQuery(sql)) {
 
 			while (rs.next()) {
+				for (Scenario sc : scenarios) {
+
+				}
 
 			}
 		} catch (SQLException e) {
