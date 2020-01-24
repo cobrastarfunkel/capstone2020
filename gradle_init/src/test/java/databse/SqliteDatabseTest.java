d8b20cd0149700cc8cc2a394b00866356c573a2d
cobrastarfunkel <iancobia@gmail.com>
2020-01-23 19:42:32 -0600

    Test cases for Scenario added

--- gradle_init/src/test/java/databse/SqliteDatabseTest.java
+++ gradle_init/src/test/java/databse/SqliteDatabseTest.java
@@ -13,12 +13,21 @@ import java.util.HashMap;
 
 import org.junit.Test;
 
+import Scenarios.Scenario;
 import database.AppSchemas.ScenariosTableSchema;
 import database.SqliteDatabase;
 
 public class SqliteDatabseTest {
 	private String dbName = "gradleDb.sqlite";
 	private SqliteDatabase testDb;
+
+	@Test
+	public void testScenarioObject() {
+		Scenario testScenario = new Scenario("scenario1.txt");
+		assertEquals("scenario1.txt", testScenario.getName());
+		assertEquals(Scenario.PATH, testScenario.getPath());
+		assertTrue("File should be byte array", testScenario.getStorableFile().getClass().equals(byte[].class));
+	}
 
 	@Test
 	public void testSchemas() {
@@ -52,7 +61,6 @@ public class SqliteDatabseTest {
 		} catch (SQLException e) {
 			fail(e.toString());
 		}
-
 	}
 
 }
