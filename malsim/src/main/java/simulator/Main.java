c16eaa4d7bb6962231015e24228348b415a565cf
cobrastarfunkel <iancobia@gmail.com>
2020-02-02 09:59:44 -0600

    Added Laynes Gui

---
+++ malsim/src/main/java/simulator/Main.java
@@ -0,0 +1,31 @@
+package simulator;
+
+import database.SqliteDatabase;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		try {
+			Parent root = FXMLLoader.load(getClass().getResource("SimulatorView.fxml"));
+			primaryStage.setTitle("Malware Simulator v1.0");
+			SqliteDatabase sqliteDB = new SqliteDatabase("guidb.sqlite");
+			sqliteDB.createDatabase();
+
+			Scene scene = new Scene(root);
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
