688bd97f24e572f7124630ec2922c054dc544509
cobrastarfunkel <iancobia@gmail.com>
2020-02-13 21:02:30 -0600

    Some css and html plus two icons I found for artifact / technique

--- malsim/src/main/java/simulator/Main.java
+++ malsim/src/main/java/simulator/Main.java
@@ -22,7 +22,6 @@ public class Main extends Application {
 		Scene scene = new Scene(root);
 		primaryStage.setScene(scene);
 		primaryStage.show();
-		// Show pull request
 	}
 
 	public static void main(String[] args) {
