57fedde186b60ed16f5b404b73bd7af7f51fbc6e
Layne <laynedillard14@gmail.com>
2020-02-18 09:22:23 -0600

    Login Screen Wireframe

--- malsim/src/main/java/simulator/Main.java
+++ malsim/src/main/java/simulator/Main.java
@@ -6,22 +6,25 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.scene.layout.GridPane;
 
 public class Main extends Application {
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
+		
 		SqliteDatabase sqliteDB = new SqliteDatabase("guidb.sqlite");
 		sqliteDB.createDatabase();
 		sqliteDB.createTables();
 
-		Parent root = FXMLLoader.load(getClass().getResource("/fxmlFiles/SimulatorView.fxml"));
+		Parent root = FXMLLoader.load(getClass().getResource("/fxmlFiles/LoginView.fxml"));
 
 		primaryStage.setTitle("Malware Simulator v1.0");
 
 		Scene scene = new Scene(root);
 		primaryStage.setScene(scene);
 		primaryStage.show();
+		
 	}
 
 	public static void main(String[] args) {
