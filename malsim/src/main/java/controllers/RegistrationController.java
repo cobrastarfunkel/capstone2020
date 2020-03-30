1589312fda3471727be645069dceff514f97ea03
cobrastarfunkel <iancobia@gmail.com>
2020-03-30 14:22:50 -0500

    Registration working

--- malsim/src/main/java/controllers/RegistrationController.java
+++ malsim/src/main/java/controllers/RegistrationController.java
@@ -2,15 +2,9 @@ package controllers;
 
 import java.io.IOException;
 import java.net.URL;
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.Statement;
-import java.util.HashMap;
 import java.util.ResourceBundle;
 
 import database.SqliteDatabase;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -18,50 +12,48 @@ import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
-import javafx.scene.control.PasswordField;
-import javafx.scene.image.ImageView;
-import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 import models.ScenarioModel;
 
 public class RegistrationController implements Initializable {
 
 	@FXML
-	package TextField user;
+	private TextField user;
 
 	@FXML
-	package TextField password;
-	
+	private TextField password;
+
 	@FXML
-	package TextField confirmPassword;
-	
+	private TextField confirmPassword;
+
+	@FXML
+	private Button loginButton;
+
 	@FXML
 	private Button registerButton;
 
 	private SqliteDatabase sqliteDB;
 	private static ScenarioModel selectedScenario;
-	
+
 	@Override
 	public void initialize(URL url, ResourceBundle rb) {
 		new FXMLLoader(getClass().getResource("/fxmlFiles/RegistrationView.fxml"));
-		
+
 		sqliteDB = new SqliteDatabase("guidb.sqlite");
 		sqliteDB.createDatabase();
-		sqlite.createTables();
+		sqliteDB.createTables();
 	}
 
 	@FXML
 	void register(ActionEvent event) {
 		try {
-			
+
 			/*
-			 * verify if user already exists in the database
-			 * if no, store username & encrypted password & salt
+			 * verify if user already exists in the database if no, store username &
+			 * encrypted password & salt
 			 */
-			
+
 			Parent root = FXMLLoader.load(getClass().getResource("/fxmlFiles/SimulatorView.fxml"));
 
 			Stage primaryStage = (Stage) loginButton.getScene().getWindow();
@@ -70,7 +62,7 @@ public class RegistrationController implements Initializable {
 			Scene scene = new Scene(root);
 			primaryStage.setScene(scene);
 			primaryStage.show();
-			
+
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
